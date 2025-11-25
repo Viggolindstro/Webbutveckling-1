53356644c7374b739d9903f74f4bca8b98b98e3f
ViggoLindstro <viggo.lindstrom@angeskola.se>
2025-11-25 11:58:26 +0100

    [Add] Kycklingko

--- Boberg/index.js
+++ Boberg/index.js
@@ -102,6 +102,17 @@ if (newsItem) {
 
 if (document.querySelector(".news-grid")) renderNews();
 if (document.querySelector(".news-content")) renderNewsDetail();
-
+ 
+function toggleText() {
+    const points = document.getElementById("points");
+    const moreText = document.getElementById("moreText");
+    const button = document.getElementById("textButton");
+ 
+    const isHidden = moreText.style.display === "none";
+ 
+    moreText.style.display = isHidden ? "inline" : "none";
+    points.style.display = isHidden ? "none" : "inline";
+    button.innerHTML = isHidden ? "Visa mindre" : "Visa fler";
+ }
  
  
